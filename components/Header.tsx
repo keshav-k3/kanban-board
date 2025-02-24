cf63d62fd5463716c9cb75658067ce6d757a3392
Keshav KK <keshavkk.musafir@gmail.com>
2025-02-24 22:24:06 +0900

    Theme Toggle setup

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+'use client';
+import React from 'react'
+import { ThemeToggle } from './ThemeToggle'
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+interface HeaderProps {
+  className?: string;
+}
+
+export const Header = ({className}: HeaderProps) => {
+  return (
+    <header
+      className={cn(
+        'fixed top-0 w-full z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60',
+        className
+      )}
+    >
+      <div className="container flex h-16 items-center justify-between">
+        <Link href="/"
+        className="flex items-center space-x-2 font-bold text-xl hover:text-primary transition-colors"
+        >
+          Kanbanmon
+        </Link>
+        <div className="border-l pl-4 dark:border-gray-800">
+            <ThemeToggle />
+        </div>
+      </div>
+    </header>
+  )
+}
