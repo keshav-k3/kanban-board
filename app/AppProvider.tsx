a30cc54df7994d3b6596a003a6870cf1e7f18bcd
Keshav KK <keshavkk.musafir@gmail.com>
2025-02-24 21:56:25 +0900

    AppProvider setup

---
+++ app/AppProvider.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/components/ThemeProvider';
+
+const queryClient = new QueryClient(); 
+
+export const AppProvider = ({children}: {children: React.ReactNode}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>{children}</ThemeProvider>
+      {children}
+      <Toaster/>
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
+  )
+}
